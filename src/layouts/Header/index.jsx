ce481db638f8409ae2c989089b654deec6551b31
sa3ina <sravnennaya@mail.ru>
2025-03-22 17:55:16 +0400

    sidebar and header

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -1,7 +1,33 @@
 import React from "react";
-
+import "./header.scss";
+import { useLocation } from "react-router-dom";
+useLocation;
+const routeNames = {
+  "/": "Dashboard",
+  "/mainpanel": "Main Panel",
+  "/classes": "Classes",
+  "/teachers": "Teachers",
+  "/students": "Students",
+  "/table": "Table",
+  "/salary": "Salary",
+  "/finance": "Finance",
+  "/stimulation": "Stimulation",
+  "/feedbacks": "Feedbacks",
+};
 const Header = () => {
-  return <div>Header</div>;
+  const location = useLocation();
+  const pageTitle = routeNames[location.pathname];
+  return (
+    <div className="header">
+      <p>{pageTitle}</p>
+      <div className="header-icons">
+        <p>EN</p>
+        <p>icon</p>
+        <p>icon</p>
+        <p>icon</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
