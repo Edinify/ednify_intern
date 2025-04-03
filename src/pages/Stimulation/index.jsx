5ccf62ffd494a098f76eb7fe9ff93d81be57daf8
jafarova1 <gultajvj@code.edu.az>
2025-04-03 12:58:33 +0400

    added

---
+++ src/pages/Stimulation/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function stimulation() {
+  return (
+    <div>
+      <div class="dashboard"></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default stimulation;
