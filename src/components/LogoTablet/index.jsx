82d9d076e1639ff515ae64e494742e40b11d3663
sa3ina <sravnennaya@mail.ru>
2025-04-28 13:43:31 +0400

    sidebar tablet size responsiveness

---
+++ src/components/LogoTablet/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logoTablet from "../../assets/images/LogoTablet.png";
+
+const LogoTablet = () => {
+  return (
+    <div className="logo-wrapper">
+      <img src={logoTablet} alt="edinify-logo.png" />
+    </div>
+  );
+};
+
+export default LogoTablet;
--- src/components/MainPanelStudentTable/index.jsx
+++ src/components/MainPanelStudentTable/index.jsx
@@ -33,16 +33,16 @@ const MainPanelStudentTable = () => {
   ];
 
   const scheduleDays = dates.map((date, index) => ({
-    day: days[index % days.length], 
+    day: days[index % days.length],
     date,
   }));
-  
+
   const getCellData = (key) => {
     const cell = schedule[key];
     if (!cell) return null;
-  
+
     const [colIndex, rowIndex] = key.split("-").map(Number);
-  
+
     return {
       subject: cell.subject,
       status: cell.status,
@@ -54,14 +54,14 @@ const MainPanelStudentTable = () => {
   };
 
   const schedule = {
-    "0-0": { subject: "Programming", status: "present" },      // Mon 10:00
-    "1-1": { subject: "Programming", status: "absent" },       // Tue 11:30
-    "2-0": { subject: "Programming", status: "present" },      // Mon 13:00
-    "3-2": { subject: "Programming", status: "absent" },       // Wed 14:30
-    "3-3": { subject: "Programming", status: "dont_attend" },  // Thu 14:30
-    "4-1": { subject: "Programming", status: "present" },      // Tue 16:00
-    "5-5": { subject: "Programming", status: "unviewed" },     // Sat 17:30
-    "3-5": { subject: "Programming", status: "canceled" },     // Sat 14:30
+    "0-0": { subject: "Programming", status: "present" }, // Mon 10:00
+    "1-1": { subject: "Programming", status: "absent" }, // Tue 11:30
+    "2-0": { subject: "Programming", status: "present" }, // Mon 13:00
+    "3-2": { subject: "Programming", status: "absent" }, // Wed 14:30
+    "3-3": { subject: "Programming", status: "dont_attend" }, // Thu 14:30
+    "4-1": { subject: "Programming", status: "present" }, // Tue 16:00
+    "5-5": { subject: "Programming", status: "unviewed" }, // Sat 17:30
+    "3-5": { subject: "Programming", status: "canceled" }, // Sat 14:30
   };
   const getStatusClass = (status) => {
     switch (status) {
@@ -89,66 +89,67 @@ const MainPanelStudentTable = () => {
     }
   };
 
-
   return (
     <div className="schedule-container">
-         <div className={`table-wrapper ${dates.length > 7 ? "scrollable" : ""}`}>
-      <table className="schedule-table">
-        <thead>
-          <tr>
-            <th></th>
-            {scheduleDays.map((item, index) => (
-              <th key={index}>
+      <div className={`table-wrapper ${dates.length > 7 ? "scrollable" : ""}`}>
+        <table className="schedule-table">
+          <thead>
+            <tr>
+              <th></th>
+              {scheduleDays.map((item, index) => (
+                <th key={index}>
                   <span className="day">{item.day}</span>
                   <span className="date">{item.date}</span>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {timeSlots.map((slot, rowIndex) => (
-            <tr key={rowIndex}>
-               <td className="time-slot">
-        <span>{slot.split("-")[0]}</span>
-        <span>-</span>
-        <span>{slot.split("-")[1]}</span>
-      </td>
-              {scheduleDays.map((_, colIndex) => {
-                const key = `${colIndex}-${rowIndex}`;
-                const cell = schedule[key];
-               
-                return (
-                  <td
-                    key={key}
-                    className={`day-off ${cell? getStatusClass(cell.status) : ""}`}
-                    onClick={() => handleCellClick(key)}
-                  >
-                    {cell && (   <div className="class-info">
-                <div className="class-name">{cell.subject}</div>
-                <div className="status-text">
-                  {cell.status.charAt(0).toUpperCase() + cell.status.slice(1)}
-                </div>
-              </div>)}
-                    
-                  </td>
-                );
-              })}
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
-  </div>
-  {open && (
-  <MainStudentModal
-    open={open}
-    setOpen={setOpen}
-    cellData={getCellData(selectedCell)}
-  />
-)}
-    
+          </thead>
+          <tbody>
+            {timeSlots.map((slot, rowIndex) => (
+              <tr key={rowIndex}>
+                <td className="time-slot">
+                  <span>{slot.split("-")[0]}</span>
+                  <span>-</span>
+                  <span>{slot.split("-")[1]}</span>
+                </td>
+                {scheduleDays.map((_, colIndex) => {
+                  const key = `${colIndex}-${rowIndex}`;
+                  const cell = schedule[key];
+
+                  return (
+                    <td
+                      key={key}
+                      className={`day-off ${
+                        cell ? getStatusClass(cell.status) : ""
+                      }`}
+                      onClick={() => handleCellClick(key)}
+                    >
+                      {cell && (
+                        <div className="class-info">
+                          <div className="class-name">{cell.subject}</div>
+                          <div className="status-text">
+                            {cell.status.charAt(0).toUpperCase() +
+                              cell.status.slice(1)}
+                          </div>
+                        </div>
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {open && (
+        <MainStudentModal
+          open={open}
+          setOpen={setOpen}
+          cellData={getCellData(selectedCell)}
+        />
+      )}
     </div>
- 
   );
 };
 
-export default MainPanelStudentTable;  
+export default MainPanelStudentTable;
